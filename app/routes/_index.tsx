28d80ce1f0ef4f5eea5947d5fa054caa8e7c8ed5
marta00084 <martvonk00084@gmail.com>
2024-02-02 18:00:33 +0100

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ export default function Homepage() {
   return (
     <div className="bg-slate-500 font-bold flex flex-1">
       <Link to="login">Login</Link>
-      <h1 className="text-lime-200 mx-2 border-2 px-2">tHomepage</h1>
+      <h1 className="text-lime-200 mx-2 border-2 px-2">tHomeepage</h1>
       <div>
         <h4>test</h4>
         <div>
